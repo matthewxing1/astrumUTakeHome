452418a8edb920ba93d308e23bdf7408d3c124ee
Matthew <matthew.xing@gmail.com>
2023-06-04 12:19:06 -0700

    added CRUD outline

---
+++ src/universities/universities.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { University } from './models/university';
+
+@Injectable()
+export class UniversitiesService {
+  private universities: University[] = [];
+
+  public getAllUniversities(): University {
+
+  }
+
+  public getUniversity(): University {
+
+  }
+
+  public createUniversity(): University {
+
+  }
+
+  public updateUniversity(): University {
+    
+  }
+}
