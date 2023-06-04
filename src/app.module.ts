2e52d2ae319ef343623ef8bcf071bb9b942299f9
Matthew <matthew.xing@gmail.com>
2023-06-04 12:05:57 -0700

    boilerplate for resolvers for universities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
-3;
