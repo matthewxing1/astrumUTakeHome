03e52abe4c8a55b5bc27f9e1b0e2764a7a9db25d
Matthew <matthew.xing@gmail.com>
2023-06-04 14:26:13 -0700

    implemented functionality for all CRUD operations using node as the backend

--- src/universities/universities.service.ts
+++ src/universities/universities.service.ts
@@ -1,23 +1,107 @@
 import { Injectable } from '@nestjs/common';
 import { University } from './models/university';
+import { CreateUniversityInput } from './dto/input/create-university.input';
+import * as fs from 'fs';
+import { GetUniversityArgs } from './dto/args/get-user.args';
+import { UpdateUniversityInput } from './dto/input/update-user.input';
 
 @Injectable()
 export class UniversitiesService {
   private universities: University[] = [];
 
-  public getAllUniversities(): University[] {
-
+  public async getAllUniversities(): Promise<University[]> {
+    try {
+      const data = await fs.promises.readFile(
+        '../../universities.json',
+        'utf8',
+      );
+      const parsedData = JSON.parse(data);
+      return parsedData.universities;
+    } catch (err) {
+      return err;
+    }
   }
 
-  public getUniversity(): University {
-
+  public async getUniversity(
+    universityId: GetUniversityArgs,
+  ): Promise<University> {
+    try {
+      const data = await fs.promises.readFile(
+        '../../universities.json',
+        'utf8',
+      );
+      const parsedData = JSON.parse(data);
+      for (const university of parsedData.universities) {
+        if (university.id === universityId.universityId) {
+          return university;
+        }
+      }
+      throw new Error(
+        `getUniversity error: No university found for id: ${universityId.universityId}`,
+      );
+    } catch (err) {
+      return err;
+    }
   }
 
-  public createUniversity(): University {
-
+  public async createUniversity(
+    createUniversity: CreateUniversityInput,
+  ): Promise<University> {
+    try {
+      const data = await fs.promises.readFile(
+        '../../universities.json',
+        'utf8',
+      );
+      const parsedData = JSON.parse(data);
+      const newUniversity: University = {
+        id: parsedData.universities.length,
+        name: createUniversity.name,
+        city: {
+          id: 1,
+          name: createUniversity.city,
+          state: {
+            id: 1,
+            name: createUniversity.state,
+          },
+        },
+      };
+      parsedData.universities.push(newUniversity);
+      fs.promises.writeFile(
+        '../../universities.json',
+        JSON.stringify(parsedData),
+      );
+      return newUniversity;
+    } catch (err) {
+      return err;
+    }
   }
 
-  public updateUniversity(): University {
-
+  public async updateUniversity(
+    updateData: UpdateUniversityInput,
+  ): Promise<University> {
+    try {
+      const data = await fs.promises.readFile(
+        '../../universities.json',
+        'utf8',
+      );
+      const parsedData = JSON.parse(data);
+      for (const university of parsedData.universities) {
+        if (university.id === updateData.id) {
+          university.name = updateData.name;
+          university.city.name = updateData.city;
+          university.city.state.name = updateData.name;
+          fs.promises.writeFile(
+            '../../universities.json',
+            JSON.stringify(parsedData),
+          );
+          return university;
+        }
+      }
+      throw new Error(
+        `updateUniversity error: No university found for id: ${updateData.id}`,
+      );
+    } catch (err) {
+      return err;
+    }
   }
 }
