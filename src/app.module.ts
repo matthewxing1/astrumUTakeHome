45fbd2302af1063ff1801789b58f5ba92712930d
Matthew <matthew.xing@gmail.com>
2023-06-04 12:18:47 -0700

    imported universities module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UniversitiesModule } from './universities/universities.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    UniversitiesModule,
   ],
   controllers: [],
   providers: [],
