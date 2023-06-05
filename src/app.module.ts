1ce081b6346c593a9b1e57a9f41b1090a272d4d3
Matthew <matthew.xing@gmail.com>
2023-06-05 09:58:18 -0700

    pseudocode for entry points

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UniversitiesModule } from './universities/universities.module';
 
+// module with v11 compatibility with Apollo Driver, and the UniversitiesModule
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UniversitiesResolver } from './universities.resolver';
 import { UniversitiesService } from './universities.service';
 
+// module with the providers of UniversitiesResolver and UniversitiesService
 @Module({
   providers: [UniversitiesResolver, UniversitiesService],
 })
